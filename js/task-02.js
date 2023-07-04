bd382513ae6351fe727970147a13eba04c650dc1
SergeLumiere <sergemrcat@gmail.com>
2023-07-04 23:31:24 +0300

    Task 02 finished

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const ingredientsHtml = ingredients.map((ingredient) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.textContent = ingredient;
+  ingredientsItemEl.classList.add("item");
+  return ingredientsItemEl;
+});
+
+ingredientsListEl.append(...ingredientsHtml);
